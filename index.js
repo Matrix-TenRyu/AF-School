141376a4372f24257e6716510a6a4a8347c4f147
Matrix-TenRyu <deadkohan@gmail.com>
2025-02-11 03:55:52 -0300

    Criation of the first page

---
+++ index.js
@@ -0,0 +1,8 @@
+// button to light and dark mode
+
+function darkMode() {
+    document.body.classList.toggle("dark-mode");
+    // const element = document.body;
+    // element.classList.toggle("dark-mode");
+
+}
